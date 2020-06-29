38fc2625d8621960e3bbbac62b1add97db0918bf
yj_gu <yj_gu@ctrip.com>
2020-06-29 12:25:27 +0800

    add build folder

---
+++ ray-tracing-web/build/precache-manifest.fec254e1eba100b783afe4fa9e4974ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd49e0818ff849879ab4",
+    "url": "/rust-ray-tracing-demo/ray-tracing-web/build/2f12cff2dc7a378c4317.module.wasm"
+  },
+  {
+    "revision": "2fba0acf73e9c2ef0aefa942efee8ea0",
+    "url": "/rust-ray-tracing-demo/ray-tracing-web/build/index.html"
+  },
+  {
+    "revision": "608cb22210051fc97ca3",
+    "url": "/rust-ray-tracing-demo/ray-tracing-web/build/static/css/3.3cb76441.chunk.css"
+  },
+  {
+    "revision": "63b2b13ed5256a3887ee",
+    "url": "/rust-ray-tracing-demo/ray-tracing-web/build/static/css/main.bd8c9455.chunk.css"
+  },
+  {
+    "revision": "bd49e0818ff849879ab4",
+    "url": "/rust-ray-tracing-demo/ray-tracing-web/build/static/js/0.d56b4d64.chunk.js"
+  },
+  {
+    "revision": "608cb22210051fc97ca3",
+    "url": "/rust-ray-tracing-demo/ray-tracing-web/build/static/js/3.7c9e6739.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rust-ray-tracing-demo/ray-tracing-web/build/static/js/3.7c9e6739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b2b13ed5256a3887ee",
+    "url": "/rust-ray-tracing-demo/ray-tracing-web/build/static/js/main.bd68855b.chunk.js"
+  },
+  {
+    "revision": "f033e91198a626d2ac1d",
+    "url": "/rust-ray-tracing-demo/ray-tracing-web/build/static/js/runtime-main.c601d113.js"
+  }
+]);
